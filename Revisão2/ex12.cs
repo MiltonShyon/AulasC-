6c38d57910950ef991bd8525ed632cf129ed7141
Milton Shyon <101666086+MiltonShyon@users.noreply.github.com>
2022-05-29 03:37:00 +0000

    ...

--- Revisão2/ex12.cs
+++ Revisão2/ex12.cs
@@ -4,12 +4,12 @@ public class Program {
   {
     Console.WriteLine("Digite dois valores inteiros separados por um operador +,-,*,/");
     string lista = Console.ReadLine();
-    Char[] sinal = {"+","-","*","/"};
-    char[] numeros = {"0","1","2","3","4","5","6","7","8","9"};
+    char[] sinal = {'+','-','*','/'};
+    char[] numeros = {'0','1','2','3','4','5','6','7','8','9'};
 
     string[] numero = lista.Split(sinal);
     string[] operador = lista.Split(numeros);
-    var total = 0;
+    
     Console.WriteLine(operador[0]);
     Console.WriteLine(numero[0]);
   }
