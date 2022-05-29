4d6d047d4cf8142c09003b0e867f49b7bac8d421
Milton Shyon <101666086+MiltonShyon@users.noreply.github.com>
2022-05-29 01:24:31 +0000

    ...

--- Revisão2/ex5.cs
+++ Revisão2/ex5.cs
@@ -9,15 +9,15 @@ public class Program {
       case 1: Console.WriteLine("O mês de Janeiro é do primeiro trimestre do ano"); break;
       case 2: Console.WriteLine("O mês de Fevereiro é do primeiro trimestre do ano"); break;
       case 3: Console.WriteLine("O mês de Março é do primeiro trimestre do ano"); break;
-      case 4: Console.WriteLine("O mês de Abril é do primeiro trimestre do ano"); break;
+      case 4: Console.WriteLine("O mês de Abril é do segundo trimestre do ano"); break;
       case 5: Console.WriteLine("O mês de Maio é do segundo trimestre do ano"); break;
       case 6: Console.WriteLine("O mês de Junho é do segundo trimestre do ano"); break;
-      case 7: Console.WriteLine("O mês de Julho é do segundo trimestre do ano"); break;
-      case 8: Console.WriteLine("O mês de Agosto é do segundo trimestre do ano"); break;
+      case 7: Console.WriteLine("O mês de Julho é do terceiro trimestre do ano"); break;
+      case 8: Console.WriteLine("O mês de Agosto é do terceiro trimestre do ano"); break;
       case 9: Console.WriteLine("O mês de Setembro é do terceiro trimestre do ano"); break;
-      case 10: Console.WriteLine("O mês de Outubro é do terceiro trimestre do ano"); break;
-      case 11: Console.WriteLine("O mês de Novembro é do terceiro trimestre do ano"); break;
-      case 12: Console.WriteLine("O mês de Dezembro é do terceiro trimestre do ano"); break;
+      case 10: Console.WriteLine("O mês de Outubro é do quarto trimestre do ano"); break;
+      case 11: Console.WriteLine("O mês de Novembro é do quarto trimestre do ano"); break;
+      case 12: Console.WriteLine("O mês de Dezembro é do quarto trimestre do ano"); break;
    }
   }
 }
