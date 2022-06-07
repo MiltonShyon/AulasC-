c677dc2fc0795e7ad599d40a12a95f90a5621177
Milton Shyon <101666086+MiltonShyon@users.noreply.github.com>
2022-06-07 12:52:05 +0000

    ...

---
+++ Lista 05/ex01.cs
@@ -0,0 +1,12 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    int i = 2;
+        for (i = 2; i <= 100; i+=2) {
+        if (i >= 0);
+        Console.WriteLine(i);
+        }
+    }
+  }
